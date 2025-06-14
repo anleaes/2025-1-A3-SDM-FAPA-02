30555e2a50ecd7db3f449da585522e482ef7754b
Leandro Oliveira <leandro.oliveira@otisoftware.com.br>
2025-06-14 20:44:22 -0300

    feat: cria urls e auctioneer

--- apps/auctioneer/urls.py
+++ apps/auctioneer/urls.py
@@ -2,10 +2,10 @@ from django.urls import path, include
 from . import views
 from rest_framework import routers
 
-app_name = 'bid'
+app_name = 'auctioneer'
 
 router = routers.DefaultRouter()
-router.register('', views.BidViewSet, basename='lance')
+router.register('', views.AuctioneerViewSet, basename='leiloeiro')
 
 urlpatterns = [
     path('', include(router.urls) )
