486ba2e7328f69e6a22395b29948c40900426de1
Leandro Oliveira <leandro.oliveira@otisoftware.com.br>
2025-06-14 20:02:40 -0300

    feat: cria models de bid

--- apps/bid/models.py
+++ apps/bid/models.py
@@ -2,3 +2,17 @@ from django.db import models
 from bidder.models import Bidder
 
 # Create your models here.
+class Bid(models.Model):
+    name = models.CharField('Nome', max_length=50)
+    amount = models.DecimalField('Valor', max_digits=10, decimal_places=2)
+    date_time = models.DateTimeField('Data do Lance', auto_now_add=True) 
+    bidder = models.ForeignKey(Bidder, on_delete=models.CASCADE)
+    # auction
+
+    class Meta:
+        verbose_name = 'Lance'
+        verbose_name_plural = 'Lances'
+        ordering =['id']
+
+    def __str__(self):
+        return self.name
