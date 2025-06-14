ff8ef15f9f03b860b7db9556be8eaed0379184bc
Leandro Oliveira <leandro.oliveira@otisoftware.com.br>
2025-06-14 20:05:39 -0300

    feat: cria restante do serializer

--- apps/bid/serializer.py
+++ apps/bid/serializer.py
@@ -1,2 +1,8 @@
 from .models import Bid
 from rest_framework import serializers
+
+class BidSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Bid
+        fields = '__all__'
+        
