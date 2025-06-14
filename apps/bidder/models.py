9f1647a80959497a3bc72b4f5ad4aec32c292945
Leandro Oliveira <leandro.oliveira@otisoftware.com.br>
2025-06-14 19:31:55 -0300

    feat: cria classe Bidder

--- apps/bidder/models.py
+++ apps/bidder/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Bidder(models.Model):
+    name = models.CharField('Nome', max_length=50)
+    email = models.EmailField('E-mail', max_length=100, unique=True)
+    phone = models.CharField('Telefone', max_length=15, blank=True, null=True, help_text='Formato: (XX) XXXXX-XXXX')
