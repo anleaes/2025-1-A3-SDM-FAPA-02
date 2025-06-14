5e84e8f49d57e37e4272ce93696547a560497731
Leandro Oliveira <leandro.oliveira@otisoftware.com.br>
2025-06-14 20:33:27 -0300

    feat: cria classe auctioneer

--- apps/auctioneer/models.py
+++ apps/auctioneer/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Auctioneer(models.Model):
+    name = models.CharField('Nome', max_length=50)
+    email = models.EmailField('E-mail', max_length=100, unique=True)
+    phone = models.CharField('Telefone', max_length=15, blank=True, null=True, help_text='Formato: (XX) XXXXX-XXXX')
+    
