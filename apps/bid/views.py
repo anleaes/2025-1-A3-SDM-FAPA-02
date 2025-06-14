f44ca4b67f5470ec36dc7868ba9abcbb6a4709b5
Leandro Oliveira <leandro.oliveira@otisoftware.com.br>
2025-06-14 20:07:58 -0300

    feat: cria restante do viewset

--- apps/bid/views.py
+++ apps/bid/views.py
@@ -3,3 +3,7 @@ from .models import Bid
 from rest_framework import viewsets
 from .serializer import BidSerializer
 # Create your views here.
+
+class BidViewSet(viewsets.ModelViewSet):
+    queryset = Bid.objects.all()
+    serializer_class = BidSerializer  
