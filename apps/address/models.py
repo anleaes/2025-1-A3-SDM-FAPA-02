fbe8348226babfb6ea5e675646fc5f978f39d7f3
Leandro Oliveira <leandro.oliveira@otisoftware.com.br>
2025-06-14 20:55:24 -0300

    feat: cria classe address em models

--- apps/address/models.py
+++ apps/address/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Address(models.Model):
+    street = models.CharField('Rua', max_length=100)
+    number = models.CharField('Número', max_length=10)
+    city = models.CharField('Cidade', max_length=100)
+    state = models.CharField('Estado', max_length=100)
