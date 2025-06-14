c2a4d2cfc9f155afac463c00b9495f55a65f442f
Leandro Oliveira <leandro.oliveira@otisoftware.com.br>
2025-06-14 20:56:22 -0300

    feat: cria classe meta

--- apps/address/models.py
+++ apps/address/models.py
@@ -6,3 +6,11 @@ class Address(models.Model):
     number = models.CharField('Número', max_length=10)
     city = models.CharField('Cidade', max_length=100)
     state = models.CharField('Estado', max_length=100)
+
+    class Meta:
+        verbose_name = 'Endereço'
+        verbose_name_plural = 'Endereços'
+        ordering =['id']
+
+    def __str__(self):
+        return self.name
