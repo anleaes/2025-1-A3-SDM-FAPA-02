abc9ae847fd78f8ce95608a9a9c3ad124432d310
Leandro Oliveira <leandro.oliveira@otisoftware.com.br>
2025-06-14 21:31:39 -0300

    feat: cria viewset do auction

--- apps/auction/views.py
+++ apps/auction/views.py
@@ -3,3 +3,7 @@ from .models import Auction
 from rest_framework import viewsets
 from .serializer import AuctionSerializer
 # Create your views here.
+
+class AuctionViewSet(viewsets.ModelViewSet):
+    queryset = Auction.objects.all()
+    serializer_class = AuctionSerializer  
