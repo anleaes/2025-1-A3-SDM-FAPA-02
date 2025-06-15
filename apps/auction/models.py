ef47d06a4c09816a3a4f31365fbb899f4cd59d22
Leandro Oliveira <leandro.oliveira@otisoftware.com.br>
2025-06-14 21:26:31 -0300

    feat: adiciona classe meta

--- apps/auction/models.py
+++ apps/auction/models.py
@@ -9,3 +9,11 @@ class Auction(models.Model):
     start_date = models.DateTimeField('Data Início')
     end_date = models.DateTimeField('Data Fim')
     auctioneer = models.ForeignKey(Auctioneer, on_delete=models.CASCADE, related_name='auctions')
+
+    class Meta:
+        verbose_name = 'Leilão'
+        verbose_name_plural = 'Leilões'
+        ordering =['id']
+
+    def __str__(self):
+        return self.name
