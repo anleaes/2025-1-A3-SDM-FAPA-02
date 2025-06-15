af280e4125e763c493262e79c3c39513dfc6b82a
Leandro Oliveira <leandro.oliveira@otisoftware.com.br>
2025-06-14 23:24:10 -0300

    feat: cria payment view set

--- apps/payment/views.py
+++ apps/payment/views.py
@@ -3,3 +3,7 @@ from .models import Payment
 from rest_framework import viewsets
 from .serializer import PaymentSerializer
 # Create your views here.
+
+class PaymentViewSet(viewsets.ModelViewSet):
+    queryset = Payment.objects.all()
+    serializer_class = PaymentSerializer 
