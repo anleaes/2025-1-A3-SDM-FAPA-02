ead5965e7de5dd7a402a6d4361b8ef43d169dac4
Leandro Oliveira <leandro.oliveira@otisoftware.com.br>
2025-06-15 17:19:36 -0300

    fix: correcoes básicas de fluxo conforme regra de negócios

--- apps/bid/models.py
+++ apps/bid/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from bidder.models import Bidder
+from item.models import Item
 
 # Create your models here.
 class Bid(models.Model):
@@ -7,7 +8,7 @@ class Bid(models.Model):
     amount = models.DecimalField('Valor', max_digits=10, decimal_places=2)
     date_time = models.DateTimeField('Data do Lance', auto_now_add=True) 
     bidder = models.ForeignKey(Bidder, on_delete=models.CASCADE)
-    # item
+    Item = models.ForeignKey(Item, on_delete=models.CASCADE, related_name='bids')
 
     class Meta:
         verbose_name = 'Lance'
